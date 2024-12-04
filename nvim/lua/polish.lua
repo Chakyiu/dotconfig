e5643e5e72f9f7be1f6f7563a1611c66882ed0ee
chakyiu <lcyharry@hotmail.com>
2024-12-04 22:28:54 +0800

    feat: allow lsp format on temp buffer & disable auto comment in new line

--- nvim/lua/polish.lua
+++ nvim/lua/polish.lua
@@ -4,6 +4,7 @@
 -- things like custom filetypes. This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
 
+-- * auto title
 local function set_titlestring()
   local cwd = vim.fn.getcwd()
   local root_dir = vim.fn.fnamemodify(cwd, ":t")
@@ -12,3 +13,9 @@ local function set_titlestring()
 end
 
 set_titlestring()
+
+-- * allow lsp format in every file type
+vim.lsp.buf.format { bufnr = 0 }
+
+-- * disable auto comment in newline
+vim.cmd "set formatoptions-=cro"

d5b18a30c829ab92c9fe5b76f39646c887122ceb
chakyiu <lcyharry@hotmail.com>
2025-04-16 23:37:51 +0800

    feat: update ghostty & lazyvim
    - Update Ghostty config
    - Update Lazyvim

--- nvim/lua/plugins/cspell.lua
+++ nvim/lua/plugins/cspell.lua
@@ -12,13 +12,14 @@ local CSPELL_CONFIG_FILES = {
 local function find_file(filename, cwd)
   ---@type string|nil
   local current_dir = cwd
-  if not current_dir then current_dir = vim.fn.getcwd() end
   local root_dir = "/"
 
   repeat
     local file_path = current_dir .. "/" .. filename
     local stat = vim.loop.fs_stat(file_path)
-    if stat and stat.type == "file" then return file_path end
+    if stat and stat.type == "file" then
+      return file_path
+    end
 
     current_dir = vim.loop.fs_realpath(current_dir .. "/..")
   until current_dir == root_dir
@@ -30,11 +31,11 @@ end
 ---@param cwd string
 ---@return string|nil
 local find_cspell_config_path = function(cwd)
-  if not cwd then cwd = vim.fn.getcwd() end
-
   for _, file in ipairs(CSPELL_CONFIG_FILES) do
     local path = find_file(file, cwd or vim.loop.cwd())
-    if path then return path end
+    if path then
+      return path
+    end
   end
   return nil
 end
@@ -45,13 +46,14 @@ end
 local function find_vscode_config_dir(cwd)
   ---@type string|nil
   local current_dir = cwd
-  if not current_dir then current_dir = vim.fn.getcwd() end
   local root_dir = "/"
 
   repeat
     local dir_path = current_dir .. "/.vscode"
     local stat = vim.loop.fs_stat(dir_path)
-    if stat and stat.type == "directory" then return dir_path end
+    if stat and stat.type == "directory" then
+      return dir_path
+    end
 
     current_dir = vim.loop.fs_realpath(current_dir .. "/..")
   until current_dir == root_dir
@@ -66,58 +68,29 @@ return {
   },
   {
     "nvimtools/none-ls.nvim",
-    config = function()
-      local null_ls = require "null-ls"
-      local cspell = require "cspell"
-
+    opts = function(_, opts)
       local config = {
         find_json = function(cwd)
           local vscode_dir = find_vscode_config_dir(cwd)
-          if vscode_dir ~= nil then return find_cspell_config_path(vscode_dir) end
+          if vscode_dir ~= nil then
+            return find_cspell_config_path(vscode_dir)
+          end
           -- not in a project with vscode, so try to find the first cspell config in the tree
           return find_cspell_config_path(cwd)
         end,
         decode_json = require("json5").parse,
       }
-
-      null_ls.setup {
-        sources = {
-          cspell.diagnostics.with {
-            config = config,
-            diagnostics_postprocess = function(diagnostic) diagnostic.severity = vim.diagnostic.severity["HINT"] end,
-          },
-        },
-      }
+      local cspell = require("cspell")
+      table.insert(
+        opts.sources,
+        cspell.diagnostics.with({
+          config = config,
+          diagnostics_postprocess = function(diagnostic)
+            diagnostic.severity = vim.diagnostic.severity["HINT"]
+          end,
+        })
+      )
+      table.insert(opts.sources, cspell.code_actions.with({ config = config }))
     end,
-    -- opts = function(_, opts)
-    --   local helpers = require("null-ls.helpers")
-    --   local cwd = vim.fn.getcwd()
-    --   local has_permission = vim.fn.filereadable(cwd) == 1
-    --
-    --   local config = {
-    --     find_json = function(cwd)
-    --       local vscode_dir = find_vscode_config_dir(cwd)
-    --       if vscode_dir ~= nil then
-    --         return find_cspell_config_path(vscode_dir)
-    --       end
-    --       -- not in a project with vscode, so try to find the first cspell config in the tree
-    --       return find_cspell_config_path(cwd)
-    --     end,
-    --     decode_json = require("json5").parse,
-    --   }
-    --
-    --   local cspell = require("cspell")
-    --
-    --   table.insert(
-    --     opts.sources,
-    --     cspell.diagnostics.with({
-    --       config = config,
-    --       diagnostics_postprocess = function(diagnostic)
-    --         diagnostic.severity = vim.diagnostic.severity["HINT"]
-    --       end,
-    --     })
-    --   )
-    --   table.insert(opts.sources, cspell.code_actions.with({ config = config }))
-    -- end,
   },
 }
